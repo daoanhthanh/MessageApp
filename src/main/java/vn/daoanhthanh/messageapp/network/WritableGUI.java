a3fd4494124a3cbed81adfe807bfd44e48dbceb3
Edward <daoanhthanh.work@gmail.com>
2021-10-31 17:28:17 +0700

    reply and send worked
    	modified:   pom.xml
    	modified:   src/main/java/vn/daoanhthanh/messageapp/gui/MainScreen.java
    	modified:   src/main/java/vn/daoanhthanh/messageapp/network/MessageListener.java
    	modified:   src/main/java/vn/daoanhthanh/messageapp/network/MessageTransmitter.java
    	modified:   src/main/java/vn/daoanhthanh/messageapp/network/WritableGUI.java
    
    	modified:   pom.xml
    	modified:   src/main/java/vn/daoanhthanh/messageapp/gui/MainScreen.java
    	modified:   src/main/java/vn/daoanhthanh/messageapp/network/MessageListener.java
    	modified:   src/main/java/vn/daoanhthanh/messageapp/network/MessageTransmitter.java
    	modified:   src/main/java/vn/daoanhthanh/messageapp/network/WritableGUI.java

--- src/main/java/vn/daoanhthanh/messageapp/network/WritableGUI.java
+++ src/main/java/vn/daoanhthanh/messageapp/network/WritableGUI.java
@@ -1,7 +1,11 @@
 package vn.daoanhthanh.messageapp.network;
 
+/**
+ * A common interface that handles writing message to UI
+ * @version 1.0
+ */
 public interface WritableGUI {
 
-    void write(String s);
+    void write(String message);
 
 }
